2f002c08b53cc71bbd171bf8bafc70512d964b90
Jakub Sokołowski <jakub@status.im>
2020-02-04 14:44:50 +0100

    main: switch data backend to consul
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- main.tf
+++ main.tf
@@ -16,11 +16,17 @@ provider "cloudflare" {
 /* DATA -----------------------------------------*/
 
 terraform {
-  backend "s3" {
-    bucket  = "tf-state-infra-nimbus"
-    key     = "infra-dapps"
-    region  = "eu-central-1"
-    encrypt = true
+  backend "consul" {
+    address = "https://consul.statusim.net:8400"
+    /* Lock to avoid syncing issues */
+    lock = true
+    /* KV store has a limit of 512KB */
+    gzip = true
+    /* WARNING This needs to be changed for every repo. */
+    path      = "terraform/nimbus/"
+    ca_file   = "ansible/files/consul-ca.crt"
+    cert_file = "ansible/files/consul-client.crt"
+    key_file  = "ansible/files/consul-client.key"
   }
 }
 
