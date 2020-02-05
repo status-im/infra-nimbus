1ef106c05b21d41fd9605e81d0ab36611f637366
Jakub Sokołowski <jakub@status.im>
2020-02-05 13:35:36 +0100

    dash: deploy node-01.aws-eu-central-1a.dash.nimbus
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ dash.tf
@@ -0,0 +1,33 @@
+module "nimbus_dashboard" {
+  source = "github.com/status-im/infra-tf-amazon-web-services"
+
+  name   = "node"
+  env    = "dash"
+  group  = "log-dash"
+  stage  = "nimbus"
+  domain = var.domain
+
+  /* Scaling */
+  host_count    = 1
+  instance_type = "t3a.medium" /* 4GB RAM at least */
+
+  /* Firewall */
+  open_tcp_ports = [
+    "80",  /* HTTP */
+    "443", /* HTTPS */
+  ]
+  
+  /* Plumbing */
+  vpc_id       = module.nimbus_network.vpc_id
+  subnet_id    = module.nimbus_network.subnet_id
+  secgroup_id  = module.nimbus_network.secgroup_id
+  keypair_name = aws_key_pair.jakubgs.key_name
+}
+
+resource "cloudflare_record" "nimbus_dashboard" {
+  zone_id = local.zones["status.im"]
+  name    = "nimbus-logs"
+  value   = module.nimbus_dashboard.public_ips[0]
+  type    = "A"
+  proxied = true
+}
