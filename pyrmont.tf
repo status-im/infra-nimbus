d0f852adce8326fa619e015067f82d34bd23185d
Jakub Sokołowski <jakub@status.im>
2020-11-20 13:51:38 +0100

    implement new layout and naming of pyrmont hosts
    This is done in advance of resizing andlayout changes.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pyrmont.tf
+++ pyrmont.tf
@@ -1,8 +1,37 @@
-module "nimbus_nodes_pyrmont" {
+module "nimbus_nodes_pyrmont_master_large" {
   source = "github.com/status-im/infra-tf-amazon-web-services"
 
-  name   = "pyrmont"
+  name   = "master-large"
   env    = "nimbus"
+  stage  = "pyrmont"
+  group  = "nimbus-slaves-pyrmont"
+  domain = var.domain
+
+  /* Scaling */
+  #instance_type = "z1d.large"
+  instance_type = "t3a.medium"
+  root_vol_size = 20
+  data_vol_size = 150
+  data_vol_type = "gp2"
+  host_count    = 2
+
+  /* Firewall */
+  open_udp_ports = local.nimbus_ports
+  open_tcp_ports = local.nimbus_ports
+
+  /* Plumbing */
+  vpc_id       = module.nimbus_network.vpc.id
+  subnet_id    = module.nimbus_network.subnets[0].id
+  secgroup_id  = module.nimbus_network.secgroup.id
+  keypair_name = aws_key_pair.jakubgs.key_name
+}
+
+module "nimbus_nodes_pyrmont_master_small" {
+  source = "github.com/status-im/infra-tf-amazon-web-services"
+
+  name   = "master-small"
+  env    = "nimbus"
+  stage  = "pyrmont"
   group  = "nimbus-slaves-pyrmont"
   domain = var.domain
 
@@ -11,7 +40,89 @@ module "nimbus_nodes_pyrmont" {
   root_vol_size = 20
   data_vol_size = 150
   data_vol_type = "gp2"
-  host_count    = 10
+  host_count    = 2
+
+  /* Firewall */
+  open_udp_ports = local.nimbus_ports
+  open_tcp_ports = local.nimbus_ports
+
+  /* Plumbing */
+  vpc_id       = module.nimbus_network.vpc.id
+  subnet_id    = module.nimbus_network.subnets[0].id
+  secgroup_id  = module.nimbus_network.secgroup.id
+  keypair_name = aws_key_pair.jakubgs.key_name
+}
+
+module "nimbus_nodes_pyrmont_devel_large" {
+  source = "github.com/status-im/infra-tf-amazon-web-services"
+
+  name   = "devel-large"
+  env    = "nimbus"
+  stage  = "pyrmont"
+  group  = "nimbus-slaves-pyrmont"
+  domain = var.domain
+
+  /* Scaling */
+  #instance_type = "z1d.large"
+  instance_type = "t3a.medium"
+  root_vol_size = 20
+  data_vol_size = 150
+  data_vol_type = "gp2"
+  host_count    = 2
+
+  /* Firewall */
+  open_udp_ports = local.nimbus_ports
+  open_tcp_ports = local.nimbus_ports
+
+  /* Plumbing */
+  vpc_id       = module.nimbus_network.vpc.id
+  subnet_id    = module.nimbus_network.subnets[0].id
+  secgroup_id  = module.nimbus_network.secgroup.id
+  keypair_name = aws_key_pair.jakubgs.key_name
+}
+
+module "nimbus_nodes_pyrmont_devel_small" {
+  source = "github.com/status-im/infra-tf-amazon-web-services"
+
+  name   = "devel-small"
+  env    = "nimbus"
+  stage  = "pyrmont"
+  group  = "nimbus-slaves-pyrmont"
+  domain = var.domain
+
+  /* Scaling */
+  instance_type = "t3a.medium"
+  root_vol_size = 20
+  data_vol_size = 150
+  data_vol_type = "gp2"
+  host_count    = 2
+
+  /* Firewall */
+  open_udp_ports = local.nimbus_ports
+  open_tcp_ports = local.nimbus_ports
+
+  /* Plumbing */
+  vpc_id       = module.nimbus_network.vpc.id
+  subnet_id    = module.nimbus_network.subnets[0].id
+  secgroup_id  = module.nimbus_network.secgroup.id
+  keypair_name = aws_key_pair.jakubgs.key_name
+}
+
+module "nimbus_nodes_pyrmont_libp2p_small" {
+  source = "github.com/status-im/infra-tf-amazon-web-services"
+
+  name   = "libp2p-small"
+  env    = "nimbus"
+  stage  = "pyrmont"
+  group  = "nimbus-slaves-pyrmont"
+  domain = var.domain
+
+  /* Scaling */
+  instance_type = "t3.medium"
+  root_vol_size = 20
+  data_vol_size = 150
+  data_vol_type = "gp2"
+  host_count    = 2
 
   /* Firewall */
   open_udp_ports = local.nimbus_ports
