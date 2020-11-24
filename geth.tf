41cb7720c57e3d814adf639a3dcec859a9fe6921
Jakub Sokołowski <jakub@status.im>
2020-11-24 13:36:44 +0100

    deploy a nimbus.geth node for goerli network
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ geth.tf
@@ -0,0 +1,25 @@
+module "nimbus_geth_goerli" {
+  source = "github.com/status-im/infra-tf-amazon-web-services"
+
+  name   = "goerli"
+  env    = "nimbus"
+  stage  = "geth"
+  group  = "nimbus-geth-goerli"
+  domain = var.domain
+
+  /* Scaling */
+  instance_type = "t3a.medium"
+  root_vol_size = 10
+  data_vol_size = 50
+  data_vol_type = "gp2"
+  host_count    = 1
+
+  /* Firewall */
+  open_tcp_ports = [ 30303 ]
+
+  /* Plumbing */
+  vpc_id       = module.nimbus_network.vpc.id
+  subnet_id    = module.nimbus_network.subnets[0].id
+  secgroup_id  = module.nimbus_network.secgroup.id
+  keypair_name = aws_key_pair.jakubgs.key_name
+}
