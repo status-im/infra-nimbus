dfa0f20bdc9c1cd275deae12c4d40fc22c07798b
Jakub Sokołowski <jakub@status.im>
2020-11-24 16:07:21 +0100

    deplo a nimbus.geth host for mainnet sync
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- geth.tf
+++ geth.tf
@@ -1,3 +1,29 @@
+module "nimbus_geth_mainnet" {
+  source = "github.com/status-im/infra-tf-amazon-web-services"
+
+  name   = "mainnet"
+  env    = "nimbus"
+  stage  = "geth"
+  group  = "nimbus-geth-mainnet"
+  domain = var.domain
+
+  /* Scaling */
+  instance_type = "t3a.medium"
+  root_vol_size = 10
+  data_vol_size = 700
+  data_vol_type = "gp2"
+  host_count    = 1
+
+  /* Firewall */
+  open_tcp_ports = [ 30303 ]
+
+  /* Plumbing */
+  vpc_id       = module.nimbus_network.vpc.id
+  subnet_id    = module.nimbus_network.subnets[0].id
+  secgroup_id  = module.nimbus_network.secgroup.id
+  keypair_name = aws_key_pair.jakubgs.key_name
+}
+
 module "nimbus_geth_goerli" {
   source = "github.com/status-im/infra-tf-amazon-web-services"
 
