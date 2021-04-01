e2749a7c9ffa5bedf5edc9ce24823cec7ff97912
Jakub Sokołowski <jakub@status.im>
2021-04-01 14:55:17 +0200

    bump Mainnet Geth node data volume to 1.8 TB
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- geth.tf
+++ geth.tf
@@ -10,7 +10,7 @@ module "nimbus_geth_mainnet" {
   /* Scaling */
   instance_type = "t3a.xlarge"
   root_vol_size = 10
-  data_vol_size = 1500
+  data_vol_size = 1800
   data_vol_type = "gp2"
   host_count    = 1
 
