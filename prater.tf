b16b9b6ef094e9b41484777853a8dd393a3deb9b
Jakub Sokołowski <jakub@status.im>
2021-07-16 15:14:30 +0200

    nimbus-prater-windows: bump to c2-standard-4, 200GB root
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- prater.tf
+++ prater.tf
@@ -167,9 +167,9 @@ module "nimbus_nodes_prater_unstable_windows" {
   open_tcp_ports = local.nimbus_ports
 
   /* Scaling */
-  type          = "n2-standard-4" /* 4 vCPUs, 16GB RAM */
+  type          = "c2-standard-4" /* 4 vCPUs, 16GB RAM */
   host_count    = 1
-  root_vol_size = 80
+  root_vol_size = 200
   root_vol_type = "pd-ssd"
 }
 
