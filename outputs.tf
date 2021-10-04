4ec73066ba751a83e641e47cd083167e115b2c80
Jakub Sokołowski <jakub@status.im>
2021-10-04 12:58:29 +0200

    add hosts outputs
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "hosts" {
+  value = merge(
+    module.nimbus_dashboard.hosts,
+    module.nimbus_log_store.hosts,
+    module.nimbus_geth_goerli.hosts,
+    module.nimbus_geth_mainnet.hosts,
+    module.nimbus_nodes_mainnet_hetzner.hosts,
+    module.nimbus_nodes_mainnet_stable_small.hosts,
+    module.nimbus_nodes_pyrmont_hetzner.hosts,
+    module.nimbus_nodes_prater_stable_large.hosts,
+    module.nimbus_nodes_prater_testing_large.hosts,
+    module.nimbus_nodes_prater_unstable_large.hosts,
+    module.nimbus_nodes_prater_unstable_libp2p_stable_large.hosts,
+    module.nimbus_nodes_prater_unstable_libp2p_unstable_large.hosts,
+    module.nimbus_nodes_prater_unstable_windows.hosts,
+    module.nimbus_nodes_prater_hetzner.hosts,
+    module.nimbus_nodes_prater_macos.hosts,
+  )
+}
