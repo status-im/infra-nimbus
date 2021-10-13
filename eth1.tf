f10eab7ed7662a4099daca59473bc30eb7f0443e
Jakub Sokołowski <jakub@status.im>
2021-10-13 20:22:45 +0200

    requirements: update to fix Hetzner data volumes
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- eth1.tf
+++ eth1.tf
@@ -1,3 +1,8 @@
+/* Hetzner AX41-NVMe
+ * AMD Ryzen 5 3600 Hexa-Core
+ * 64 GB DDR4 RAM
+ * 2 x 512 GB NVMe SSD
+ * 1 x 2 TB NVMe SSD */
 module "nimbus_eth1_node_hetzner" {
   source = "./modules/dummy-module"
 
