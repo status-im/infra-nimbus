761b0d7e2b1bc88cffc6918df663e6d444d6df27
Jakub Sokołowski <jakub@status.im>
2022-02-03 20:30:20 +0100

    terraform: inherit aws provider version from module
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- versions.tf
+++ versions.tf
@@ -5,13 +5,12 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = " = 2.21.0"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = " = 3.46.0"
-    }
     pass = {
       source  = "camptocamp/pass"
       version = " = 2.0.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
     }
   }
 }
