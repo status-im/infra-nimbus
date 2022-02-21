3acbf34f7079d350334d20dc13a6a9304f847105
Jakub Sokołowski <jakub@status.im>
2022-02-21 13:52:15 +0100

    add new mainnet-01.he-eu-hel1.nimbus.openeth host
    https://github.com/status-im/infra-nimbus/issues/80
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ openeth.tf
@@ -0,0 +1,18 @@
+/* Hetzner AX41-NVMe
+ * AMD Ryzen 5 3600 Hexa-Core
+ * 64 GB DDR4 RAM
+ * 2 x 512 GB NVMe SSD */
+module "nimbus_openeth_mainnet" {
+  source = "./modules/dummy-module"
+
+  name   = "mainnet"
+  env    = "nimbus"
+  stage  = "openeth"
+  group  = "nimbus-openeth-mainnet"
+  domain = var.domain
+
+  ips = [
+    "95.217.120.250", # mainnet-01.he-eu-hel1.nimbus.openeth
+  ]
+}
+
