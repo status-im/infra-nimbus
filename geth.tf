68f370d447ee5a106a3590155669536a0660ce4c
Jakub Sokołowski <jakub@status.im>
2022-05-17 21:37:40 +0200

    nimbus.geth: increase data volume to 200 GB
    Full sync for Goerli reached 123GB.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- geth.tf
+++ geth.tf
@@ -36,7 +36,7 @@ module "nimbus_geth_goerli" {
   /* Scaling */
   type          = "t3a.large"
   root_vol_size = 10
-  data_vol_size = 150
+  data_vol_size = 200
   data_vol_type = "gp2"
   host_count    = 1
 
