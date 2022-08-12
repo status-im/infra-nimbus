7d0029b9e2de74f2d44e53ab9298af5a6a6ffb20
Jakub Sokołowski <jakub@status.im>
2022-08-12 10:53:47 +0200

    drop unused ropsten-01.aws-eu-central-1a.nimbus.geth
    https://github.com/status-im/infra-nimbus/issues/117
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- geth.tf
+++ geth.tf
@@ -49,29 +49,3 @@ module "nimbus_geth_goerli" {
   secgroup_id  = module.nimbus_network.secgroup.id
   keypair_name = aws_key_pair.jakubgs.key_name
 }
-
-module "nimbus_geth_ropsten" {
-  source = "github.com/status-im/infra-tf-amazon-web-services"
-
-  name   = "ropsten"
-  env    = "nimbus"
-  stage  = "geth"
-  group  = "nimbus-geth-ropsten"
-  domain = var.domain
-
-  /* Scaling */
-  type          = "t3a.large"
-  root_vol_size = 10
-  data_vol_size = 250
-  data_vol_type = "gp2"
-  host_count    = 1
-
-  /* Firewall */
-  open_tcp_ports = [30303]
-
-  /* Plumbing */
-  vpc_id       = module.nimbus_network.vpc.id
-  subnet_id    = module.nimbus_network.subnets[0].id
-  secgroup_id  = module.nimbus_network.secgroup.id
-  keypair_name = aws_key_pair.jakubgs.key_name
-}
