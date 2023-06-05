0441d524a997f4fcd34f2129b5e62967704e34a7
Jakub Sokołowski <jakub@status.im>
2023-06-05 12:08:43 +0200

    drop linux-01.he-eu-hel1.nimbus.sepolia host
    https://github.com/status-im/infra-nimbus/issues/132
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- sepolia.tf
+++ sepolia.tf
@@ -1,19 +1,3 @@
-module "nimbus_nodes_sepolia_hetzner" {
-  source = "github.com/status-im/infra-tf-dummy-module"
-
-  name   = "linux"
-  env    = "nimbus"
-  stage  = "sepolia"
-  group  = "nimbus-sepolia-linux"
-  region = "eu-hel1"
-  prefix = "he"
-  domain = var.domain
-
-  ips = [
-    "65.21.89.157", # linux-01.he-eu-hel1.nimbus.sepolia
-  ]
-}
-
 module "nimbus_nodes_sepolia_innova" {
   source = "github.com/status-im/infra-tf-dummy-module"
 
@@ -34,7 +18,7 @@ module "nimbus_nodes_sepolia_innova" {
 resource "cloudflare_record" "unstable_sepolia_beacon_api" {
   zone_id = local.zones["nimbus.team"]
   name    = "unstable.sepolia.beacon-api"
-  value   = module.nimbus_nodes_sepolia_hetzner.public_ips[0]
+  value   = module.nimbus_nodes_sepolia_innova.public_ips[0]
   type    = "A"
   proxied = false
 }
