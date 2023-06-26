aa80ab67df90b8860a12333635d373d19384cce5
Jakub Sokołowski <jakub@status.im>
2023-06-26 21:59:37 +0200

    nimbus.fluffy: add two new hosts from InnovaHosting
    https://github.com/status-im/infra-nimbus/issues/132
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- fluffy.tf
+++ fluffy.tf
@@ -18,3 +18,25 @@ module "nimbus_nodes_fluffy_hetzner" {
     "65.108.42.174", # metal-02.he-eu-hel1.nimbus.fluffy
   ]
 }
+
+/* Innova Hosting
+/* MOB: ProLiant DL380p Gen8
+ * CPU: Intel Xeon E5-2667 v3 @ 3.20GHz
+ * MEM: 62 GB DDR3
+ * SSD: 1x400 GB, 1x1.6 TB */
+module "nimbus_nodes_fluffy_innova" {
+  source = "github.com/status-im/infra-tf-dummy-module"
+
+  name   = "metal"
+  env    = "nimbus"
+  stage  = "fluffy"
+  group  = "nimbus-fluffy-metal"
+  region = "eu-mda1"
+  prefix = "ih"
+  domain = var.domain
+
+  ips = [
+    "194.33.40.238", # metal-01.ih-eu-mda1.nimbus.fluffy
+    "194.33.40.239", # metal-02.ih-eu-mda1.nimbus.fluffy
+  ]
+}
