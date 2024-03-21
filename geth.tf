10ff179299bb08432205de75d42849c5be41ba33
Jakub Sokołowski <jakub@status.im>
2024-03-21 16:53:36 +0100

    nimbus.holesky: add Geth hosts for MacOS and Windows
    No Windows host with beacon nodes currently.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- geth.tf
+++ geth.tf
@@ -23,3 +23,19 @@ module "nimbus_geth_mainnet" {
   secgroup_id  = module.nimbus_network.secgroup.id
   keypair_name = aws_key_pair.jakubgs.key_name
 }
+
+module "nimbus_geth_holesky" {
+  source = "github.com/status-im/infra-tf-dummy-module"
+
+  name   = "holesky"
+  env    = "nimbus"
+  stage  = "geth"
+  group  = "nimbus-geth-holesky"
+  region = "eu-mda1"
+  prefix = "ih"
+
+  ips = [
+    "194.33.40.232", # holesky-01.ih-eu-mda1.nimbus.geth
+    "194.33.40.233", # holesky-02.ih-eu-mda1.nimbus.geth
+  ]
+}
--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,18 @@
 output "hosts" {
   value = merge(
     module.nimbus_dashboard.hosts,
-    module.nimbus_log_store.hosts,
-    module.nimbus_geth_mainnet.hosts,
-    module.nimbus_nodes_fluffy_innova.hosts,
     module.nimbus_eth1_node_innova.hosts,
+    module.nimbus_geth_holesky.hosts,
+    module.nimbus_geth_mainnet.hosts,
+    module.nimbus_log_store.hosts,
+    module.nimbus_nodes_fluffy_innova.hosts,
+    module.nimbus_nodes_holesky_innova_erigon.hosts,
+    module.nimbus_nodes_holesky_innova_geth.hosts,
+    module.nimbus_nodes_holesky_innova_macm2.hosts,
+    module.nimbus_nodes_holesky_innova_neth.hosts,
     module.nimbus_nodes_mainnet_innova.hosts,
     module.nimbus_nodes_mainnet_stable_small.hosts,
     module.nimbus_nodes_prater_innova.hosts,
     module.nimbus_nodes_sepolia_innova.hosts,
-    module.nimbus_nodes_holesky_innova_geth.hosts,
-    module.nimbus_nodes_holesky_innova_erigon.hosts,
-    module.nimbus_nodes_holesky_innova_neth.hosts,
-    module.nimbus_nodes_holesky_innova_macm2.hosts,
   )
 }
