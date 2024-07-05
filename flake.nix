8617f3dfbfd3b5926fbb19a46d56d69d40cbf5d5
Jakub Sokołowski <jakub@status.im>
2024-07-05 13:40:18 +0200

    flake: add flake.nix and lock
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "infra-shell";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      stableSystems = ["x86_64-linux"  "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      forAllSystems = nixpkgs.lib.genAttrs stableSystems;
+      pkgsFor = nixpkgs.lib.genAttrs stableSystems (
+        system: import nixpkgs { inherit system; config.allowUnfree = true; }
+      );
+    in rec {
+      devShells = forAllSystems (system: let
+        pkgs = pkgsFor.${system};
+      in {
+        default = let
+          pythonPkgs = pkgs.python310.withPackages (
+            _: with (pkgs.python310Packages); [
+              ipython pyyaml jinja2 PyGithub
+              pyopenssl cryptography
+            ]
+          );
+        in pkgs.mkShellNoCC {
+          packages = with pkgs.buildPackages; [
+            # misc
+            git openssh jq fzf silver-searcher
+            # networking
+            curl nmap nettools dnsutils
+            # infra
+            terraform ansible_2_16 pythonPkgs
+            # security
+            pass bitwarden-cli yubikey-manager pwgen
+            # cloud
+            aliyun-cli awscli doctl google-cloud-sdk
+            hcloud s3cmd scaleway-cli
+          ];
+
+          shellHook = ''
+            ./ansible/roles.py --check || \
+              echo -e '\nWARNING: Your role versions appear to be incorrect!' >&2
+          '';
+        };
+      });
+    };
+}
