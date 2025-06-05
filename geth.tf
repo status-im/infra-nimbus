725ee501619dfd00a475d287babd83206efe9c47
Jakub Sokołowski <jakub@status.im>
2025-06-05 12:54:13 +0200

    mainnet: decomission unused AWS Geth host
    Appears to have no purpose. Was deployed as Infura fallback.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- geth.tf
+++ geth.tf
@@ -1,29 +1,3 @@
-module "nimbus_geth_mainnet" {
-  source = "github.com/status-im/infra-tf-amazon-web-services"
-
-  name   = "mainnet"
-  env    = "nimbus"
-  stage  = "geth"
-  group  = "nimbus-geth-mainnet"
-
-  /* Scaling */
-  type          = "t3a.xlarge"
-  root_vol_size = 10
-  data_vol_size = 2500
-  data_vol_type = "gp2"
-  host_count    = 1
-
-  /* Firewall */
-  open_tcp_ports = [30303]
-  open_udp_ports = [30303]
-
-  /* Plumbing */
-  vpc_id       = module.nimbus_network.vpc.id
-  subnet_id    = module.nimbus_network.subnets[0].id
-  secgroup_id  = module.nimbus_network.secgroup.id
-  keypair_name = aws_key_pair.jakubgs.key_name
-}
-
 module "nimbus_geth_holesky" {
   source = "github.com/status-im/infra-tf-dummy-module"
 
--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,6 @@ output "hosts" {
     module.nimbus_eth1_node_innova.hosts,
     module.nimbus_eth1_node_benchmark.hosts,
     module.nimbus_geth_holesky.hosts,
-    module.nimbus_geth_mainnet.hosts,
     module.nimbus_log_store.hosts,
     module.nimbus_nodes_fluffy_innova.hosts,
     module.nimbus_nodes_holesky_innova_geth.hosts,
