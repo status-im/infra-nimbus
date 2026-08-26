4a2885979f7f409af7fa825303fcb0dc40cf8eee
Jakub Sokołowski <jakub@status.im>
2026-08-26 15:22:59 +0200

    flake: add community.hashi_vault Ansible collection
    Useful for access to Hashicorp Vault related tasks.
    
    https://github.com/status-im/infra-template/pull/50
    https://github.com/status-im/infra-hq/issues/302
    https://github.com/ansible-collections/community.hashi_vault/pull/504

--- flake.nix
+++ flake.nix
@@ -17,10 +17,24 @@
         default = let
           pythonPkgs = pkgs.python313.withPackages (
             _: with (pkgs.python313Packages); [
-              ipython pyyaml jinja2 pygithub
+              ipython ansible ansible-core pyyaml jinja2 pygithub
               pyopenssl cryptography hvac pysocks
             ]
           );
+
+          ansibleCollections = pkgs.linkFarm "ansible-collections" [
+            {
+              name = "ansible_collections/community/hashi_vault";
+              # Not yet supported upstream in ansible-collections:
+              # https://github.com/ansible-collections/community.hashi_vault/pull/504
+              path = pkgs.fetchFromGitHub {
+                owner = "status-im";
+                repo = "community.hashi_vault";
+                rev = "add-mtls";
+                hash = "sha256-hjaLq2VHV1SJcVrf1qNIbpKFqG7TnGMg9a6tup1ytYQ=";
+              };
+            }
+          ];
         in pkgs.mkShellNoCC {
           packages = with pkgs.buildPackages; let
             # Optional pkgs.nix for customizing repo dev shell.
@@ -32,13 +46,15 @@
             # networking
             curl nmap nettools dnsutils
             # infra
-            terraform ansible_2_18 pythonPkgs
+            terraform pythonPkgs
             # security
             gopass vault yubikey-manager pwgen
             # cloud
             aliyun-cli awscli doctl google-cloud-sdk
             hcloud s3cmd scaleway-cli
           ] ++ extraPkgs;
+
+          ANSIBLE_COLLECTIONS_PATH = "${ansibleCollections}";
 
           shellHook = ''
             make checks
