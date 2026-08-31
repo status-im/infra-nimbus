edfc4efbfec2f16e71f83e659c7031706e5460e8
Jakub Sokołowski <jakub@status.im>
2026-08-31 10:30:38 +0200

    logs: rename Nimbus hosts to use logs as stage name
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- logs.tf
+++ logs.tf
@@ -1,17 +1,17 @@
-module "nimbus_log_store" {
+module "nimbus_logs_store" {
   source = "github.com/status-im/infra-tf-dummy-module"
 
   name   = "store"
-  env    = "logs"
-  stage  = "nimbus"
-  group  = "logs.nimbus"
+  env    = "nimbus"
+  stage  = "logs"
+  group  = "nimbus-logs-store"
   region = "eu-hel1"
   prefix = "he"
   type   = "AX101"
 
   ips = [
-    "65.108.226.62", /* store-01.he-eu-hel1.logs.nimbus */
-    "65.109.62.247", /* store-02.he-eu-hel1.logs.nimbus */
-    "65.109.49.101", /* store-03.he-eu-hel1.logs.nimbus */
+    "65.108.226.62", /* store-01.he-eu-hel1.nimbus.logs */
+    "65.109.62.247", /* store-02.he-eu-hel1.nimbus.logs */
+    "65.109.49.101", /* store-03.he-eu-hel1.nimbus.logs */
   ]
 }
